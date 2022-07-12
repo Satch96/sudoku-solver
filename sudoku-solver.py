e9a9b069a84d777b9abb7d22223804594eb75bdd
Satch96 <satch196@gmail.com>
2022-07-12 19:07:50 +0100

    added main file

---
+++ sudoku-solver.py
@@ -0,0 +1,25 @@
+test_sudoku = [
+    [3,0,0,0],
+    [0,2,0,1],
+    [1,0,2,0],
+    [0,0,0,3]
+]
+
+answer = [
+    [3,1,4,2],
+    [4,2,3,1],
+    [1,3,2,4],
+    [2,4,1,3]
+]
+
+
+#TO DO
+
+#iterate over array and look for 0's
+
+#horizontal line checking function
+
+#vertical line checking function
+
+#grid line checking function
+
