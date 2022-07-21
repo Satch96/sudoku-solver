8451363419e3b370eddaa2928a60bc4a66e134cd
Satch96 <satch196@gmail.com>
2022-07-21 21:16:49 +0100

    started to combine all functions in one

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -2,7 +2,6 @@
 from math import sqrt
 
 
-
 test_sudoku = [
     [3,0,0,0],
     [0,2,0,1],
@@ -17,8 +16,9 @@ answer = [
     [2,4,1,3]
 ]
 
+# get size of smaller grids
+mini_grid_size = int(sqrt(len(test_sudoku)))
 
-#TO DO
 
 zero_positions = []
 
@@ -32,8 +32,10 @@ for row, i in enumerate(test_sudoku):
 
 # horizontal checker
 # for i in zero_positions:
+    
 #     number = 1
 #     iterator = 0
+
 #     while iterator < len(test_sudoku):
 #         for x in test_sudoku[i['y']]:
 #             if x == number:
@@ -42,30 +44,80 @@ for row, i in enumerate(test_sudoku):
 #                 break
 #             iterator +=1
 #     test_sudoku[i['y']][i['x']] = number
-#     number += 1
-#     iterator = 0
+
+
+
 
 #vertical checker
 # for i in zero_positions:
+    # number = 1
+    # iterator = 0
+    # vertical_list = [row[i['x']] for row in test_sudoku]
+    # while iterator < len(test_sudoku):
+    #     for x in vertical_list:
+    #         if x == number:
+    #             number +=1
+    #             iterator = 0
+    #             break
+    #         iterator +=1
+    # test_sudoku[i['y']][i['x']] = number
+
+
+
+
+# # grid checker
+
+
+# for i in zero_positions:
+
+#     #get coordinates that need to be checked
+#     x_start = int(i['x']/mini_grid_size) * mini_grid_size
+#     y_start = int(i['y']/mini_grid_size) * mini_grid_size
+
+#     #iterate over mini grids
+#     mini_grid_numbers = []
+
+#     for j in range(mini_grid_size):
+#         for k in range(mini_grid_size):
+#             mini_grid_numbers.append(test_sudoku[y_start + j][x_start + k])
+    
 #     number = 1
 #     iterator = 0
 #     while iterator < len(test_sudoku):
-#         vertical_list = [row[i['x']] for row in test_sudoku]
-#         for x in vertical_list:
+#         for x in mini_grid_numbers:
 #             if x == number:
 #                 number +=1
 #                 iterator = 0
 #                 break
 #             iterator +=1
 #     test_sudoku[i['y']][i['x']] = number
-#     number += 1
-#     iterator = 0
 
 
-# grid checker
-mini_grid_size = int(sqrt(len(test_sudoku)))
+
+def checking(type):
+
+    number = 1
+    iterator = 0
+
+    while iterator < len(test_sudoku):
+        for x in type:
+            if x == number:
+                number +=1
+                iterator = 0
+                break
+            iterator +=1
+    
+    return number
+
+#combine all 3 checking functions:
 
 for i in zero_positions:
+    
+    number = 1
+    iterator = 0
+    
+    horizontal_list = test_sudoku[i['y']]
+    vertical_list = [row[i['x']] for row in test_sudoku]
 
     #get coordinates that need to be checked
     x_start = int(i['x']/mini_grid_size) * mini_grid_size
@@ -77,18 +129,17 @@ for i in zero_positions:
     for j in range(mini_grid_size):
         for k in range(mini_grid_size):
             mini_grid_numbers.append(test_sudoku[y_start + j][x_start + k])
-    
-    number = 1
-    iterator = 0
+
     while iterator < len(test_sudoku):
-        for x in mini_grid_numbers:
+        for x in test_sudoku[i['y']]:
             if x == number:
                 number +=1
                 iterator = 0
                 break
             iterator +=1
-    test_sudoku[i['y']][i['x']] = number
-    number += 1
-    iterator = 0
 
-print(test_sudoku)
+
+    test_sudoku[i['y']][i['x']] = number
+
+    # with each checker see how much bigger the number is than 1, and reduce while loop by that much
+                          
