d822c79a840d9ec8099cef730a77c8b27538aafe
Satchin Mistry <satch196@gmail.com>
2022-07-24 13:22:18 +0100

    finished combining checkers, just need to add recursion

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -3,18 +3,17 @@ from math import sqrt
 
 
 test_sudoku = [
-    [3,0,0,0],
-    [0,2,0,1],
-    [1,0,2,0],
-    [0,0,0,3]
+    [5,0,9,0,0,0,0,0,7],
+    [0,8,0,0,1,0,5,2,0],
+    [0,0,3,0,8,4,0,0,1],
+    [0,9,0,7,0,0,0,0,2],
+    [4,0,0,0,5,0,3,9,0],
+    [8,0,2,1,0,0,0,0,4],
+    [0,0,0,3,0,2,0,0,5],
+    [0,4,0,0,0,0,7,0,0],
+    [1,0,7,0,9,0,0,8,0]
 ]
 
-answer = [
-    [3,1,4,2],
-    [4,2,3,1],
-    [1,3,2,4],
-    [2,4,1,3]
-]
 
 # get size of smaller grids
 mini_grid_size = int(sqrt(len(test_sudoku)))
@@ -30,76 +29,13 @@ for row, i in enumerate(test_sudoku):
         
 # checking functions
 
-# horizontal checker
-# for i in zero_positions:
-    
-#     number = 1
-#     iterator = 0
+def checking(type, number):
 
-#     while iterator < len(test_sudoku):
-#         for x in test_sudoku[i['y']]:
-#             if x == number:
-#                 number +=1
-#                 iterator = 0
-#                 break
-#             iterator +=1
-#     test_sudoku[i['y']][i['x']] = number
-
-
-
-
-#vertical checker
-# for i in zero_positions:
-    # number = 1
-    # iterator = 0
-    # vertical_list = [row[i['x']] for row in test_sudoku]
-    # while iterator < len(test_sudoku):
-    #     for x in vertical_list:
-    #         if x == number:
-    #             number +=1
-    #             iterator = 0
-    #             break
-    #         iterator +=1
-    # test_sudoku[i['y']][i['x']] = number
-
-
-
-
-# # grid checker
-
-
-# for i in zero_positions:
-
-#     #get coordinates that need to be checked
-#     x_start = int(i['x']/mini_grid_size) * mini_grid_size
-#     y_start = int(i['y']/mini_grid_size) * mini_grid_size
-
-#     #iterate over mini grids
-#     mini_grid_numbers = []
-
-#     for j in range(mini_grid_size):
-#         for k in range(mini_grid_size):
-#             mini_grid_numbers.append(test_sudoku[y_start + j][x_start + k])
-    
-#     number = 1
-#     iterator = 0
-#     while iterator < len(test_sudoku):
-#         for x in mini_grid_numbers:
-#             if x == number:
-#                 number +=1
-#                 iterator = 0
-#                 break
-#             iterator +=1
-#     test_sudoku[i['y']][i['x']] = number
-
-
-
-def checking(type):
-
-    number = 1
     iterator = 0
+    #get difference between current number and 1 so you know how much to iterate
+    diff = number - 1
 
-    while iterator < len(test_sudoku):
+    while iterator < len(test_sudoku) - diff:
         for x in type:
             if x == number:
                 number +=1
@@ -109,12 +45,12 @@ def checking(type):
     
     return number
 
+
+    
+
 #combine all 3 checking functions:
 
 for i in zero_positions:
-    
-    number = 1
-    iterator = 0
     
     horizontal_list = test_sudoku[i['y']]
     vertical_list = [row[i['x']] for row in test_sudoku]
@@ -129,17 +65,37 @@ for i in zero_positions:
     for j in range(mini_grid_size):
         for k in range(mini_grid_size):
             mini_grid_numbers.append(test_sudoku[y_start + j][x_start + k])
+    
+    number = 1
 
-    while iterator < len(test_sudoku):
-        for x in test_sudoku[i['y']]:
-            if x == number:
-                number +=1
-                iterator = 0
-                break
-            iterator +=1
+    iterator2 = 0
 
+    while iterator2 < 3:
+
+        new_number = checking(horizontal_list, number)
+        if new_number != number:
+            number = new_number
+            iterator2 = 1
+        else:
+            number = new_number
+            iterator2 += 1
+        new_number = checking(vertical_list, number)
+        if new_number != number:
+            number = new_number
+            iterator2 = 1
+        else:
+            number = new_number
+            iterator2 += 1
+        new_number = checking(mini_grid_numbers, number)
+        if new_number != number:
+            number = new_number
+            iterator2 = 1
+        else:
+            number = new_number
+            iterator2 += 1
 
     test_sudoku[i['y']][i['x']] = number
 
-    # with each checker see how much bigger the number is than 1, and reduce while loop by that much
-                          
+
+
+print(test_sudoku)
