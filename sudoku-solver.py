ec1b52213c4195f196f8d7bf6f93fa3edd2a96cc
Satch96 <satch196@gmail.com>
2022-08-05 17:41:55 +0100

    tidied up code a bit

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -3,15 +3,15 @@ from math import sqrt
 
 
 test_sudoku = [
-    [5,0,9,0,0,0,0,0,7],
-    [0,8,0,0,1,0,5,2,0],
-    [0,0,3,0,8,4,0,0,1],
-    [0,9,0,7,0,0,0,0,2],
-    [4,0,0,0,5,0,3,9,0],
-    [8,0,2,1,0,0,0,0,4],
-    [0,0,0,3,0,2,0,0,5],
-    [0,4,0,0,0,0,7,0,0],
-    [1,0,7,0,9,0,0,8,0]
+    [7,6,0,2,0,0,0,0,4],
+    [0,0,1,0,4,0,0,8,0],
+    [0,9,0,3,0,1,0,2,5],
+    [8,0,0,0,1,0,0,0,7],
+    [0,4,0,0,0,2,0,5,0],
+    [0,0,5,4,0,9,8,0,0],
+    [0,0,7,0,6,0,0,0,9],
+    [3,0,0,8,0,0,0,7,0],
+    [0,2,0,0,9,0,1,0,0]
 ]
 
 
@@ -19,16 +19,20 @@ test_sudoku = [
 mini_grid_size = int(sqrt(len(test_sudoku)))
 
 
+#find all positions where there are zeroes
 zero_positions = []
 
-#iterate over array and look for 0's
 for row, i in enumerate(test_sudoku):
     for column, j in enumerate(i):
         if j == 0:
             zero_positions.append({'x': column,'y': row})
-        
-# checking functions
 
+
+# number of zero positions in sudoku
+zero_positions_length = len(zero_positions)
+
+
+# checking function
 def checking(type, number):
 
     if len(type) == 0:
@@ -48,16 +52,13 @@ def checking(type, number):
         
     return number
 
-# need to add something adds number for a particular zero position. so during the checks can also check that number
+# array for al numbers tried for a certain position. needed for backtracking
 numbers_tried = []
-
-# goes through all zero positions and checks to see if number already used
-zero_positions_length = len(zero_positions)
 
 for i in range(zero_positions_length):
     numbers_tried.append([])
 
-
+# main function
 count = 0
 while count < zero_positions_length:
 
@@ -77,40 +78,23 @@ while count < zero_positions_length:
         for k in range(mini_grid_size):
             mini_grid_numbers.append(test_sudoku[y_start + j][x_start + k])
     
+    # array of lists that need checking
+    list_of_lists = [horizontal_list, vertical_list, mini_grid_numbers, numbers_tried[count]]
+    
     number = 1
     iterator2 = 0
 
     #this makes sure it does all of the checking functions at least once
     while iterator2 < 4:
 
-        new_number = checking(horizontal_list, number)
-        if new_number != number:
-            number = new_number
-            iterator2 = 1
-        else:
-            number = new_number
-            iterator2 += 1
-        new_number = checking(vertical_list, number)
-        if new_number != number:
-            number = new_number
-            iterator2 = 1
-        else:
-            number = new_number
-            iterator2 += 1
-        new_number = checking(mini_grid_numbers, number)
-        if new_number != number:
-            number = new_number
-            iterator2 = 1
-        else:
-            number = new_number
-            iterator2 += 1
-        new_number = checking(numbers_tried[count], number)
-        if new_number != number:
-            number = new_number
-            iterator2 = 1
-        else:
-            number = new_number
-            iterator2 += 1
+        for t in list_of_lists:
+            new_number = checking(t, number)
+            if new_number != number:
+                number = new_number
+                iterator2 = 1
+            else:
+                number = new_number
+                iterator2 += 1
 
     if number > 9:
         test_sudoku[i['y']][i['x']] = 0
